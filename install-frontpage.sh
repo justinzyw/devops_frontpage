63aca1e01cb348067035eb9c5ddc8e5113719b0a
Justin <justin.zy.wang@gmail.com>
2017-10-28 07:34:02 +0800

    added frontpage sh

---
+++ install-frontpage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Fetch the variables
+. parm.txt
+
+# function to get the current time formatted
+currentTime()
+{
+  date +"%Y-%m-%d %H:%M:%S";
+}
+
+#echo ---$(currentTime)---populate the volumes---
+#to zip, use: sudo tar zcvf devops_portainer_volume.tar.gz /var/nfs/volumes/devops_frontpage*
+#sudo tar zxvf devops_frontpage_volume.tar.gz -C /
+
+
+echo ---$(currentTime)---create frontpage service---
+sudo docker service create -d \
+--publish $FRONTPAGE_PORT:80 \
+--name devops-frontpage \
+--network $NETWORK_NAME \
+--replicas 1 \
+$FRONTPAGE_IMAGE
